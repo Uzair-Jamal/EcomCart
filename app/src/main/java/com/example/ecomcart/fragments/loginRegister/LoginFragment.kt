4c07b03c8dcf9f63f5d6e1af7974eca5df9de680
My Pc <syeduzair99jamal@gmail.com>
2024-07-15 16:45:06 +0500

    Till Login design.

---
+++ app/src/main/java/com/example/ecomcart/fragments/loginRegister/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.example.ecomcart.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.ecomcart.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+}
---
+++ app/src/main/java/com/example/ecomcart/fragments/loginRegister/RegisterFragment.kt
@@ -0,0 +1,8 @@
+package com.example.ecomcart.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.ecomcart.R
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+}
